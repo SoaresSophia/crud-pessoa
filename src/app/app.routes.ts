295731efd70581c12c888edce0d280a059368ecd
Sophia Costa <aishspo@gmail.com>
2025-05-19 17:49:27 -0300

    feat: primeiro contato com app-component e app-routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { ListarPessoaComponent } from './pages/listar-pessoa/listar-pessoa.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'pessoas/listar', pathMatch: 'full'},
+  { path: 'pessoas', redirectTo: 'pessoas/listar'},
+  { path: 'pessoas/listar', component: ListarPessoaComponent},
+  
+  { path: 'login', component: LoginComponent},
+];
